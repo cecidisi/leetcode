7af982d66534dd2444069464e9efaf56c5abfe22
cecidisi <cecidisi@users.noreply.github.com>
2019-09-01 19:31:31 +0200

    Update 100.same_tree.py

--- 100.same_tree.py
+++ 100.same_tree.py
@@ -1,23 +1,3 @@
-# Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
-
-# For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
-
-#     1
-#    / \
-#   2   2
-#  / \ / \
-# 3  4 4  3
- 
-
-# But the following [1,2,2,null,3,null,3] is not:
-
-#     1
-#    / \
-#   2   2
-#    \   \
-#    3    3
-
-### Solution: https://leetcode.com/problems/symmetric-tree/solution/
 
 # Definition for a binary tree node.
 # class TreeNode(object):
